45380b0587f6cfadd28cf8b688a661e9429d51fb
kajyr <carlo.panzi@gmail.com>
2015-01-28 17:43:36 +0100

    initial parsing

--- app/app.js
+++ app/app.js
@@ -9,25 +9,34 @@
 	var fs = require('fs');
 
 	var $main = $('#main');
-	$main.html('<pre />');
-	$output = $main.find('pre');
 
 	function getUserHome() {
 		return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+	}
+
+	function parseAndPrint(savegame) {
+		$main.html('');
+		for (var key in savegame) {
+			if(savegame.hasOwnProperty(key)){
+				$main.append('<h2>' + key + '</h2>');
+			}
+		}
 	}
 
 	$('header button').on('click', function(e) {
 		var slot = $(e.currentTarget).data('slot');
 		var path = getUserHome() + save_paths[slot];
 
-		
 		fs.readFile(path, 'utf8', function (err,data) {
 			if (err) {
 				return $output.html(err.message);
 			}
+			if (data === '') {
+				return $main.html('<p>This savegame seems empty</p>');
+			}
 			data = data.replace(/\n/g, ' ');
 			var savegame = JSON.parse(data);
-			$output.html(savegame.length);
+			parseAndPrint(savegame);
 		});
 
 	})
